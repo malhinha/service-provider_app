c679fb4027a0babc289eed677812d6f3a77b228d
John Malhinha <john.malhinha@gmail.com>
2021-08-30 22:24:55 -0400

    Tightened up nav styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,16 +3,23 @@ import { Link } from 'react-router-dom';
 
 const NavBar = props => {
 	return (
-		<div className="col-2 bg-light">
-			<div className="logo mt-3 mb-3">Dog Walkin</div>
-			<ul className="d-flex flex-column justify-content-around">
-				{props.routes.map(({ key, path }) => (
-					<li key={key} className="navigation">
-						<Link to={path}>{key}</Link>
-					</li>
-				))}
-			</ul>
-		</div>
+		<>
+			<div className="col-3 bg-light p-5" height="100%">
+				<div
+					className="d-flex flex-column justify-content-center align-items-center"
+					height="100%"
+				>
+					<div className="logo mb-5">Dog Walkin</div>
+					<ul className="d-flex flex-column justify-content-around">
+						{props.routes.map(({ key, path }) => (
+							<li key={key} className="navigation p-3 mb-1">
+								<Link to={path}>{key}</Link>
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
+		</>
 	);
 };
 
