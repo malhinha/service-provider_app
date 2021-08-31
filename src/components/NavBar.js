7ff625803dabb1ff9f057df5095e80c48c4f86ff
John Malhinha <john.malhinha@gmail.com>
2021-08-30 23:03:01 -0400

    More navigation cleanup

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,16 +5,18 @@ const NavBar = props => {
 	return (
 		<>
 			<div className="col-3 bg-light p-5" height="100%">
-				<div
-					className="d-flex flex-column justify-content-center align-items-center"
-					height="100%"
-				>
+				<div className="d-flex flex-column justify-content-center align-items-center">
 					<div className="logo mb-5">Dog Walkin</div>
-					<ul className="d-flex flex-column justify-content-around">
+					<ul
+						className="d-flex flex-column justify-content-around"
+						width="100%"
+					>
 						{props.routes.map(({ key, path }) => (
-							<li key={key} className="navigation p-3 mb-1">
-								<Link to={path}>{key}</Link>
-							</li>
+							<Link key={key} to={path}>
+								<li className="navigation p-3 mb-1" width="100%">
+									{key}
+								</li>
+							</Link>
 						))}
 					</ul>
 				</div>
